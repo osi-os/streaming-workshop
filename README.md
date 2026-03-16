9f54c9a280bf01cb2919cd89fc10381b1ac1a2f5
osi-os <oosman489@gmail.com>
2026-03-16 22:28:28 +0000

    Working on week 7 homework

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # streaming-workshop
 streaming workshop for DEZoomcamp
+
+## Seventh Module & Homework in the DE Zoomcamp Series
+
+1. What version of Redpanda are you running?
+
+  docker exec -it streaming-workshop-redpanda-1 rpk version
+  rpk version: v25.3.9
+
+2. The time it takes to send the entire dataset and flush: ~ 10 seconds
+
+3. How many trips have trip_distance > 5?
+    8506
---
+++ notebooks/hwmodels.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+import json
+import dataclasses
+
+
+@dataclass
+class Ride:
+    lpep_pickup_datetime: object
+    lpep_dropoff_datetime: object
+    PULocationID: int
+    DOLocationID: int
+    passenger_count: int
+    trip_distance: float
+    tip_amount: float
+    total_amount: float
+
+def ride_from_row(row):
+    return Ride(
+        lpep_pickup_datetime=row['lpep_pickup_datetime'],
+        lpep_dropoff_datetime=row['lpep_dropoff_datetime'],
+        PULocationID=int(row['PULocationID']),
+        DOLocationID=int(row['DOLocationID']),
+        passenger_count=int(row['passenger_count']),
+        trip_distance=float(row['trip_distance']),
+        tip_amount=float(row['tip_amount']),
+        total_amount=float(row['total_amount']),
+    )
+
+
+
+def ride_serializer(ride):
+    ride_dict = dataclasses.asdict(ride)
+    ride_json = json.dumps(ride_dict).encode('utf-8')
+    return ride_json
+
+def ride_deserializer(data):
+    json_str = data.decode('utf-8')
+    ride_dict = json.loads(json_str)
+    return Ride(**ride_dict)
